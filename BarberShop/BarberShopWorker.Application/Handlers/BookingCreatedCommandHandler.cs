0e3e9ee2976ffa036612046368827e2626699193
luisfff <luisfb8@gmail.com>
2023-01-07 23:09:57 +0100

    fix: comment out IBookingRepository

--- BarberShop/BarberShopWorker.Application/Handlers/BookingCreatedCommandHandler.cs
+++ BarberShop/BarberShopWorker.Application/Handlers/BookingCreatedCommandHandler.cs
@@ -9,12 +9,12 @@ namespace BarberShopWorker.Application.Handlers
     public class BookingCreatedCommandHandler : IRequestHandler<BookingCreateCommand>
     {
         private readonly ILogger<BookingCreatedCommandHandler> _logger;
-        private readonly IBookingRepository _bookingRepository;
+       // private readonly IBookingRepository _bookingRepository;
 
-        public BookingCreatedCommandHandler(ILogger<BookingCreatedCommandHandler> logger, IBookingRepository bookingRepository)
+        public BookingCreatedCommandHandler(ILogger<BookingCreatedCommandHandler> logger)
         {
             _logger = logger;
-            _bookingRepository = bookingRepository;
+       //     _bookingRepository = bookingRepository;
         }
 
         public async Task<Unit> Handle(BookingCreateCommand request, CancellationToken cancellationToken)
@@ -26,7 +26,7 @@ namespace BarberShopWorker.Application.Handlers
                 BookingDateTime = request.BookingDateTime
             };
 
-            await _bookingRepository.Create(booking);
+        //    await _bookingRepository.Create(booking);
 
             _logger.LogInformation("---- Received message: {Message} ----", request.UserId);
             return await Task.FromResult(Unit.Value);
